5735018632afc426040a205fa74f24ff83d9fb28
Caleb Mingle <me@caleb.io>
2014-07-26 05:01:17 -0400

    Use existing virtualenv prompt function instead of my own.

--- inc/rprompt.sh
+++ inc/rprompt.sh
@@ -1,5 +1,2 @@
-function _virtual_env() {
-    [ $VIRTUAL_ENV ] && echo 'Virtual Env: ('`basename $VIRTUAL_ENV`') '
-}
+export RPROMPT='$(_vi_status)%{$(echotc UP 1)%}$(virtualenv_prompt_info) $(git_prompt_status) ${_return_status}%{$(echotc DO 1)%}'
 
-export RPROMPT='$(_vi_status)%{$(echotc UP 1)%}$(_virtual_env) $(git_prompt_status) ${_return_status}%{$(echotc DO 1)%}'
