f22f7eff265b95c736aa92e7c4732cfd5923ca73
Caleb Mingle <me@caleb.io>
2014-07-26 02:03:51 -0400

    Add .zshrc to the bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
