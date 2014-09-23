b5e942eb61628d4a14e51f6700fd5332e957d280
Caleb Mingle <mingle@uber.com>
2014-09-23 04:34:47 +0000

    Reorder stuff in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 . ~/dotfiles/update.sh
+
+# zsh config
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# vim Configuration
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# tmux Configuration
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# git Configuration
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# ack configuration
 ln -sf ~/dotfiles/.ackrc ~/.ackrc
