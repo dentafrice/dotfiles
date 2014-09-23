26a4bb73fad974b733a013c5f00e24de19ea0dc8
Caleb Mingle <mingle@uber.com>
2014-09-23 04:56:37 +0000

    more prezto foo

--- install_prezto.sh
+++ install_prezto.sh
@@ -1,4 +1,5 @@
+#!/bin/zsh
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/prezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto/runcoms/^README.md(.N); do
   ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
