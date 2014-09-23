864a1b43d135dede5ab4b3651f7884b4b683c2ac
Caleb Mingle <mingle@uber.com>
2014-09-23 04:59:03 +0000

    Add zprezto symlink

--- install_prezto.sh
+++ install_prezto.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 setopt EXTENDED_GLOB
+
+ln -sf ~/dotfiles/.zprezto ~/.zprezto
+
 for rcfile in "${ZDOTDIR:-$HOME}"/dotfiles/.zprezto/runcoms/^README.md(.N); do
   ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
