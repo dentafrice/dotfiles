5c8915dd880eb78927a0b33853a4c4487100f1d8
Caleb Mingle <mingle@uber.com>
2014-09-26 20:26:59 +0000

    Add sst for supervisorctl status and tls for tmux ls

--- inc/zsh/aliases.zsh
+++ inc/zsh/aliases.zsh
@@ -3,8 +3,10 @@ alias dotfiles="cd ~/dotfiles/"
 if [[ $IS_LINUX -eq 1 ]]; then
     alias work="tmux attach -t work"
     alias sr="supervisorctl restart"
+    alias sst="supervisorctl status"
 fi
 
 alias v='fasd -e vim'
 alias tkill='tmux kill-session -t'
 alias ta='tmux attach -t'
+alias tls='tmux ls'
