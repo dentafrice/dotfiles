8c9c482292ae4c8cfaf6bde1782a98651a96f94e
Caleb Mingle <mingle@uber.com>
2014-11-06 09:28:51 +0000

    Add clip aliases

--- inc/zsh/aliases.zsh
+++ inc/zsh/aliases.zsh
@@ -4,6 +4,9 @@ if [[ $IS_LINUX -eq 1 ]]; then
     alias work="tmux attach -t work"
     alias sr="supervisorctl restart"
     alias sst="supervisorctl status"
+    alias clip="nc --send-only localhost 8377"
+else
+    alias clip="nc localhost 8377"
 fi
 
 alias v='fasd -e vim'
