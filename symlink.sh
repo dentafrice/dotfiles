366c5f066ace37cb7242dd29a58d1d6f7a50a9c7
Caleb Mingle <mingle@uber.com>
2015-10-30 19:26:57 -0700

    Remove X and dunst

--- symlink.sh
+++ symlink.sh
@@ -17,20 +17,11 @@ ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 # ack configuration
 ln -sf ~/dotfiles/.ackrc ~/.ackrc
 
-# x
-ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
-ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
-
 # i3
 rm -rf ~/.i3
 ln -sf ~/dotfiles/.i3 ~/.i3
-
 ln -sf ~/dotfiles/.i3/.i3status.conf ~/.i3status.conf
 
 # bin stuff
 rm -rf ~/bin
 ln -sf ~/dotfiles/bin ~/bin
-
-# dunst
-mkdir -p ~/.config/dunst
-ln -sf ~/dotfiles/dunstrc ~/.config/dunst/dunstrc
