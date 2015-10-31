4be8a70390b1229d7891846cb67fb6481ac7a75d
Caleb Mingle <mingle@uber.com>
2015-10-30 19:38:25 -0700

    Get rid of gitconfig

--- symlink.sh
+++ symlink.sh
@@ -5,9 +5,6 @@ rm -rf ~/.vim
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
-# git Configuration
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
 # bin stuff
 rm -rf ~/bin
 ln -sf ~/dotfiles/bin ~/bin
